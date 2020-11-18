77208f17dbd280761cb820535addc657bedcd229
mikio-korean22 <mikio.fullmoon.korea@gmail.com>
2020-11-18 15:34:34 +0000

    #

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_034134) do
+ActiveRecord::Schema.define(version: 2020_11_18_145702) do
 
   create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "quiz_id"
