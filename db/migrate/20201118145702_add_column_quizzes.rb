47be18bd72b3e7a5688a4622f34285cb08ddf9a9
mikio-korean22 <mikio.fullmoon.korea@gmail.com>
2020-11-19 04:54:20 +0000

    #

--- db/migrate/20201118145702_add_column_quizzes.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnQuizzes < ActiveRecord::Migration[5.2]
-  def change
-    
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_18_145702) do
+ActiveRecord::Schema.define(version: 2020_11_19_044248) do
 
   create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "quiz_id"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_145702) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "level_id"
+    t.integer "level_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
