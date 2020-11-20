33971776efbf5bcf18d454904b1142d40b0dafc4
mikio-korean22 <mikio.fullmoon.korea@gmail.com>
2020-11-20 05:32:10 +0000

    #

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_063432) do
+ActiveRecord::Schema.define(version: 2020_11_19_224638) do
 
   create_table "questions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "quiz_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_11_19_063432) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "questions", "quizzes"
